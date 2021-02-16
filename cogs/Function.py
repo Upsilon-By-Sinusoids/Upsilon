f75fba1f7a85cfbe24ce3f246be0847089e86db4
Upsilon-By-Sinusoids <79071980+Upsilon-By-Sinusoids@users.noreply.github.com>
2021-02-17 03:41:45 +0530

    Update Function.py

--- cogs/Function.py
+++ cogs/Function.py
@@ -43,6 +43,7 @@ class Function(commands.Cog):
         await ctx.channel.purge(limit=amount+1)
 
     @commands.command()
+    @commands.has_role('Strong Nuclear Force')
     async def nuke(self, ctx, channel: discord.TextChannel):
         await channel.delete()
         await ctx.send(f'A nuke has been dropped on #{channel}')
