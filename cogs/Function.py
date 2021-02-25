083edebd7736d07f5a1760347b0fc38216ef77b5
Upsilon-By-Sinusoids <79071980+Upsilon-By-Sinusoids@users.noreply.github.com>
2021-02-25 21:39:46 +0530

    Update Function.py

--- cogs/Function.py
+++ cogs/Function.py
@@ -44,7 +44,7 @@ class Function(commands.Cog):
         await ctx.channel.purge(limit=amount+1)
 
     @commands.command()
-    @commands.has_role('Strong Nuclear Force')
+    #@commands.has_role('Strong Nuclear Force')
     @commands.has_role('Captain')
     async def nuke(self, ctx, channel: discord.TextChannel):
         try:
