49128b9364f3f471cfe2c6efcf7c804a2f85a118
Upsilon-By-Sinusoids <79071980+Upsilon-By-Sinusoids@users.noreply.github.com>
2021-04-19 02:02:26 +0530

    Update Music.py

--- cogs/Music.py
+++ cogs/Music.py
@@ -1,4 +1,26 @@
-
+import discord 
+from discord.ext import commands, tasks
+from itertools import cycle
+import time
+import youtube_dl
+from async_timeout import timeout
+import discord.utils
+import math
+import random
+import asyncio
+import functools
+import json
+import requests
+from chess import *
+import os
+#import chess.svg
+#import cairo
+#import cairosvg
+#import logging
+import ctypes
+import ctypes.util
+from better_profanity import profanity
+from datetime import date 
 
 
 
