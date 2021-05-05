054131473f1b33ca0495150c727f4444ff97c9fa
Upsilon-By-Sinusoids <79071980+Upsilon-By-Sinusoids@users.noreply.github.com>
2021-05-05 14:39:51 +0530

    Update Function.py

--- cogs/Function.py
+++ cogs/Function.py
@@ -35,10 +35,10 @@ class Function(commands.Cog):
     async def on_ready(self):
         print('Bot is online')
         
-    @commands.Cog.listener()
-    async def on_message(message):
-        if message.content.startswith('owner')
-            await message.send(f'message.guild.owner_id')
+    #@commands.Cog.listener()
+    #async def on_message(message):
+    #    if message.content.startswith('owner')
+    #        await message.send(f'message.guild.owner_id')
   
 
     @commands.Cog.listener()
