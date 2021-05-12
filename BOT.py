49a1023916144454bd66d7df21dd5976199ab7f5
Upsilon-By-Sinusoids <79071980+Upsilon-By-Sinusoids@users.noreply.github.com>
2021-05-13 00:53:26 +0530

    Update BOT.py

--- BOT.py
+++ BOT.py
@@ -5,6 +5,14 @@ from discord.ext import commands, tasks
 from itertools import cycle
 from decouple import config
 import json
+import discord
+import logging
+
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
 
 intents = discord.Intents().all()
 
