0f021deac041fa9ecc2b439d9b104a2a761b90d7
Upsilon-By-Sinusoids <79071980+Upsilon-By-Sinusoids@users.noreply.github.com>
2021-05-13 13:22:28 +0530

    Update Function.py

--- cogs/Function.py
+++ cogs/Function.py
@@ -110,7 +110,7 @@ class Function(commands.Cog):
     async def servers(self, ctx):
         """This command is exclusively for the owner of Upsilon"""
         member = "ΔΨφ#6251"
-        t = client.guilds()
+        t = client.fetch_guilds
             #channel = await member.create_dm()
         print(t)
             #await channel.send(guild)
