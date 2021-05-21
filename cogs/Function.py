23d487b2b27b2b647c4a2f2e9d6502e503828c2d
Upsilon-By-Sinusoids <79071980+Upsilon-By-Sinusoids@users.noreply.github.com>
2021-05-21 12:17:19 +0530

    Update Function.py

--- cogs/Function.py
+++ cogs/Function.py
@@ -27,7 +27,7 @@ from datetime import date
 bot = discord.Client
 
 async def log(message):
-    channel = client.get_channel(844996703034540073)
+    channel = bot.get_channel(844996703034540073)
     await channel.send(message)
 
 
