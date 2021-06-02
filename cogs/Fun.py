93ce8d6920c897d4b59a67fb1521cf37355cca8e
deltapsifi <74860497+deltapsifi@users.noreply.github.com>
2021-06-03 00:58:40 +0530

    Update Fun.py

--- cogs/Fun.py
+++ cogs/Fun.py
@@ -23,6 +23,8 @@ from better_profanity import profanity
 from datetime import date 
 
 
+kill = ["https://img.cinemablend.com/filter:scale/quill/c/d/2/8/8/a/cd288aec327829a7589fb0d06cf3474c35352782.gif?mw=600", "https://cdn.discordapp.com/attachments/784494481159618560/849313765703024670/ezgif.com-gif-maker.gif"]
+
 
 class Miscellaneous(commands.Cog):
     
