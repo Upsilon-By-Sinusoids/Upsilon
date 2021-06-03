ed66277c5a7f145daa7c77f6f4ea6d699c21c469
deltapsifi <74860497+deltapsifi@users.noreply.github.com>
2021-06-03 18:49:11 +0530

    Create owner.py

---
+++ cogs/owner.py
@@ -0,0 +1,48 @@
+import discord 
+from discord.ext import commands, tasks
+from itertools import cycle
+import time
+import youtube_dl
+from async_timeout import timeout
+import discord.utils
+import math
+import random
+import asyncio
+import functools
+import json
+import requests
+from chess import *
+import os
+#import chess.svg
+#import cairo
+#import cairosvg
+#import logging
+import ctypes
+import ctypes.util
+from better_profanity import profanity
+from datetime import date 
+
+class Owner(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+        
+
+    #events
+        
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print('Owner cmds are working')
+        
+    @commands.Cog.listener()
+    async def on_message(message):
+        if message.guild.name == "WILFLEAXʷˣ #YENİDEN":
+            print(await message.channel.create_invite(max_age=300))
+    
+
+        
+        
+        
+def setup(client):
+    client.add_cog(Owner(client))
+        
+        
