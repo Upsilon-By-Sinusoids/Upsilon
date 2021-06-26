3dd8f1b4f919ba2c334b9bc7671c4e95d7bc59a8
deltapsifi <74860497+deltapsifi@users.noreply.github.com>
2021-06-26 15:33:53 +0530

    Update BOT.py

--- BOT.py
+++ BOT.py
@@ -84,5 +84,7 @@ for filename in os.listdir('cogs'):
     if filename.endswith('.py'):
         client.load_extension(f'cogs.{filename[:-3]}')
         
-TOKEN = config('TOKEN')
+#TOKEN = config('TOKEN')
+TOKEN = open("token.gitignore", "r")
+TOKEN.read()
 client.run(TOKEN)
