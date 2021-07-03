44c124a2e00604fba5efc1c48da34ae68adabd55
deltapsifi <74860497+deltapsifi@users.noreply.github.com>
2021-07-03 16:57:37 +0530

    Update Moderation.py

--- cogs/Moderation.py
+++ cogs/Moderation.py
@@ -101,7 +101,6 @@ class Moderation(commands.Cog):
     @commands.command(aliases=["takedown","exile"])
     async def arrest(self, ctx, member : discord.Member):
         for i in (member.roles):
-            i+=1
             await i.delete
         embed = discord.Embed(title=f"{member.name}, you are under arrest.",description="""You have the right to remain silent,
 anything you say or do can be used against you in the court of law.""", color=discord.Color(red))
