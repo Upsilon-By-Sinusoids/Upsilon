c5726d6cc8c8ac8c515e0869c052012f47d3822d
deltapsifi <74860497+deltapsifi@users.noreply.github.com>
2021-07-06 00:18:45 +0530

    Create ErrorHandler.py

---
+++ cogs/ErrorHandler.py
@@ -0,0 +1,46 @@
+import discord 
+from discord.ext import commands, tasks
+from itertools import cycle
+import time
+import youtube_dl
+from async_timeout import timeout
+import discord.utils
+import math
+import random
+import asyncio
+import functools
+import json
+import requests
+from chess import *
+import os
+#import chess.svg
+#import cairo
+#import cairosvg
+#import logging
+import ctypes
+import ctypes.util
+from better_profanity import profanity
+from datetime import date 
+
+x = ["WARNING, UNAUTHORISED COMMAND USAGE by {}", "**WARNING**, __Possible Anarchyst found in Server --> {ctx.author.mention}__"]
+
+
+class ErrorHandler(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    @commands.Cog.listener()
+    async def on_command_error(self, ctx, error):
+        r = random.choice(x)
+        if isinstance(error, commands.MissingPermissions):
+            await ctx.send(r.format(ctx.author.mention))
+        elif isinstance(error, commands.MemberNotFound):
+            await ctx.send(f"Sorry buddy, This person does not exist. Promise me you will stop taking drugs to prevent hallucinations.")
+        else:
+            await ctx.send(f"An error occured, please consider taking help from the support server.")
+         
+        
+        
+def setup(client):
+    client.add_cog(ErrorHandler(client))
+        
