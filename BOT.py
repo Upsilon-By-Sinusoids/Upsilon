3f976722525089bddf5aff250bd73804a7bf044b
deltapsifi <74860497+deltapsifi@users.noreply.github.com>
2021-07-13 19:55:25 +0530

    Update BOT.py

--- BOT.py
+++ BOT.py
@@ -19,8 +19,12 @@ class CustomHelpCommand(commands.HelpCommand):
 
     async def send_bot_help(self, mapping):
         embed=discord.Embed(title="This is the Help Command", description=f"Use `{get_prefix}help <module>` to gain more information about that module", color=discord.Color.blurple())
+        print(mapping)
+        print("\n \n")
         for cog in mapping:
             print(type(cog))
+            print("\n")
+            print(cog)
             embed.add_field(name=f"{cog.qualified_name}:", value=f"{mapping[cog]}",inline=False)
         await self.get_destination().send(embed=embed)
 
