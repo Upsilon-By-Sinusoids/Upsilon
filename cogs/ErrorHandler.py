2bd65f2d6333184c9defbb5db999ddaa1b28c0e0
deltapsifi <74860497+deltapsifi@users.noreply.github.com>
2021-07-14 20:11:37 +0530

    Update ErrorHandler.py

--- cogs/ErrorHandler.py
+++ cogs/ErrorHandler.py
@@ -22,7 +22,7 @@ import ctypes.util
 from better_profanity import profanity
 from datetime import date 
 
-x = ["WARNING, UNAUTHORISED COMMAND USAGE by {}", "**WARNING**, __Possible Anarchyst found in Server --> {ctx.author.mention}__"]
+x = ["WARNING, UNAUTHORISED COMMAND USAGE by {ctx.author.mention}", "**WARNING**, __Possible Anarchyst found in Server --> {ctx.author.mention}__"]
 
 
 class ErrorHandler(commands.Cog):
@@ -33,7 +33,7 @@ class ErrorHandler(commands.Cog):
     async def on_command_error(self, ctx, error):
         r = random.choice(x)
         if isinstance(error, commands.MissingPermissions):
-            await ctx.send(r.format(ctx.author.mention))
+            await ctx.send(r)
         elif isinstance(error, commands.MemberNotFound):
             await ctx.send(f"Sorry buddy, This person does not exist. Promise me you will stop taking drugs to prevent hallucinations.")
         else:
