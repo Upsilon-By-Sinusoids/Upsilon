d0d7398a8df93d328d026ac9559161ac56e52442
deltapsifi <74860497+deltapsifi@users.noreply.github.com>
2021-07-19 17:56:04 +0530

    Update Miscellaneous.py

--- cogs/Miscellaneous.py
+++ cogs/Miscellaneous.py
@@ -48,6 +48,20 @@ class Miscellaneous(commands.Cog):
             embed=discord.Embed(title=f"{user} was killed by {ctx.author}", description='', color=0xe91e63)
             embed.set_image(url=(url))
             await ctx.send(embed=embed)
+      
+    @cog_ext.cog_slash(name="kill", description="Your personal Death Note!")
+    async def _kill(self, ctx, user : discord.Member):
+        if user == ctx.author:
+            embed=discord.Embed(title=f"{user}, I know it seems tempting to take the easy way out, my advice is that you listen to some good ol' songs by typing .play [song name].", description='', color=0xe74c3c)
+            #embed.set_image(url=("https://tenor.com/MTjK.gif"))
+            await ctx.send(embed=embed)
+            await asyncio.sleep(1.5)
+            await ctx.send(embed=discord.Embed(title="Go on then, what are you waiting for?",description="",color=0xe74c3c))
+        else:
+            url = random.choice(kill)
+            embed=discord.Embed(title=f"{user} was killed by {ctx.author}", description='', color=0xe91e63)
+            embed.set_image(url=(url))
+            await ctx.send(embed=embed)
         
     
    
