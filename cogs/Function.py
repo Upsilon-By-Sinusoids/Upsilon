b5a843b6d067633690287c56ed8642b8ca309b79
deltapsifi <74860497+deltapsifi@users.noreply.github.com>
2021-08-09 14:55:27 +0530

    Update Function.py

--- cogs/Function.py
+++ cogs/Function.py
@@ -73,10 +73,10 @@ class Function(commands.Cog):
        
     @commands.Cog.listener()
     async def on_message_edit(self, message_before, message_after):
-        if message.channel.id == 845245176888688660 or message.channel.id == 865789884035498004:
+        if message_before.channel.id == 845245176888688660 or message_before.channel.id == 865789884035498004:
             return
         me = f"``{message_before.content}`` was changed to ``{message_after.content}`` by {message_before.member.name} in the channel {message_before.channel} of the server {message_before.channel.guild}"
-        chan = self.client.get_channel(865789884035498004)
+        chan = self.client.get_channel(874221498225819648)
         await chan.send(me)
 
     @commands.Cog.listener()
