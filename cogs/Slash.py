89682c75bd34eacf90b60f905ca75bfc75843533
Upsilon-By-Sinusoids <79071980+Upsilon-By-Sinusoids@users.noreply.github.com>
2021-09-12 01:06:50 +0530

    Update Slash.py

--- cogs/Slash.py
+++ cogs/Slash.py
@@ -24,6 +24,19 @@ from datetime import date
 
 kill = ["https://img.cinemablend.com/filter:scale/quill/c/d/2/8/8/a/cd288aec327829a7589fb0d06cf3474c35352782.gif?mw=600", "https://cdn.discordapp.com/attachments/784494481159618560/849313765703024670/ezgif.com-gif-maker.gif"]
 
+clid = config('clid')
+clis = config('clis')
+pasw = config('pasw')
+
+reddit = asyncpraw.Reddit(client_id=clid,
+                    client_secret=clis,
+                    username="SpecialistNo7996",
+                    password=pasw,
+                    user_agent="pegasus")
+
+color = [discord.Color.blurple(),discord.Color.greyple(),discord.Color.orange(),discord.Color.red(),discord.Color.blue(),discord.Color.dark_purple(),discord.Color.dark_magenta(),discord.Color.green(),discord.Color.purple(),discord.Color.teal()]
+
+
 
 class Slash(commands.Cog):
 
@@ -152,6 +165,38 @@ class Slash(commands.Cog):
         await ctx.delete(message)
         channel = self.client.get_channel(845245176888688660)
         await channel.send(m)
+       
+    @cog_ext.cog_slash(name="kill", description="Your personal Death Note!")
+    async def _kill(self, ctx, user : discord.Member):
+        if user == ctx.author:
+            embed=discord.Embed(title=f"{user}, I know it seems tempting to take the easy way out, my advice is that you listen to some good ol' songs by typing .play [song name].", description='', color=0xe74c3c)
+            #embed.set_image(url=("https://tenor.com/MTjK.gif"))
+            await ctx.send(embed=embed)
+            await asyncio.sleep(1.5)
+            await ctx.send(embed=discord.Embed(title="Go on then, what are you waiting for?",description="",color=0xe74c3c))
+        else:
+            url = random.choice(kill)
+            embed=discord.Embed(title=f"{user} was killed by {ctx.author}", description='', color=0xe91e63)
+            embed.set_image(url=(url))
+            await ctx.send(embed=embed)
+          
+       
+    @cog_ext.cog_slash(name="meme", description="Sends a reddit meme")
+    async def meme(self, ctx):
+        subreddit = await reddit.subreddit("memes")
+        l = []
+
+        async for i in subreddit.hot(limit=80):
+            l.append(i)
+
+        rand = random.choice(l)
+        tit = rand.title
+        ur = rand.url
+        e = discord.Embed(title=f"{tit}", description="", color=random.choice(color))
+        e.set_image(url=ur)
+
+        await ctx.send(embed=e)
+            
     
 def setup(client):
     client.add_cog(Slash(client))
