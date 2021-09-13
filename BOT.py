3e2634e0ad4934b3f194c76d996dc04c3c8d3fe4
deltapsifi <74860497+deltapsifi@users.noreply.github.com>
2021-09-13 19:09:09 +0530

    Update BOT.py

--- BOT.py
+++ BOT.py
@@ -32,7 +32,7 @@ class CustomHelpCommand(commands.HelpCommand):
             embed.add_field(name=f"{cog.qualified_name}:", value=f"{[command.name for command in mapping[cog]]}",inline=False)
         embed.add_field(name="Vote for me !!!", value=f"https://top.gg/bot/784473379183788055 \nhttps://discordbotlist.com/bots/upsilon/upvote", inline=False)
         embed.add_field(name="Join the support server for any help", value="https://discord.gg/aXVWmDxRmF", inline=False)
-        embed.set_footer(text="Type ``.help <command>`` for more information on a command. \nYou can also type ``.help <category>`` for more info on a category. \nCreated by ΔΨφ#0001 (he's the Devil, so don't play with him 😈)")
+        embed.set_footer(text="Type ``.help <command>`` for more information on a command. \nYou can also type ``.help <category>`` for more info on a category. \nCreated by ΔΨφ#4993 (he's the Devil, so don't play with him 😈)")
         await self.get_destination().send(embed=embed)
 
     async def send_cog_help(self, cog):
@@ -41,7 +41,7 @@ class CustomHelpCommand(commands.HelpCommand):
             embed.add_field(name=f"{command.name}", value=f"{command.help} \nUsage: {command.signature}", inline=False)
         embed.add_field(name="Vote for me !!!", value=f"https://top.gg/bot/784473379183788055 \nhttps://discordbotlist.com/bots/upsilon/upvote", inline=False)
         embed.add_field(name="Join the support server for any help", value="https://discord.gg/aXVWmDxRmF", inline=False)
-        embed.set_footer(text="Created by ΔΨφ#0001 (he's the Devil, so don't play with him 😈)")
+        embed.set_footer(text="Created by ΔΨφ#4993 (he's the Devil, so don't play with him 😈)")
         await self.get_destination().send(embed=embed)
 
     async def send_command_help(self, command):
@@ -54,7 +54,7 @@ class CustomHelpCommand(commands.HelpCommand):
             embed.add_field(name=f"Usage:", value=f"{command.signature}")
         embed.add_field(name="Vote for me !!!", value=f"https://top.gg/bot/784473379183788055 \nhttps://discordbotlist.com/bots/upsilon/upvote", inline=False)
         embed.add_field(name="Join the support server for any help", value="https://discord.gg/aXVWmDxRmF", inline=False)
-        embed.set_footer(text="Created by ΔΨφ#0001 (he's the Devil, so don't play with him 😈)")
+        embed.set_footer(text="Created by ΔΨφ#4993 (he's the Devil, so don't play with him 😈)")
         await self.get_destination().send(embed=embed)
 
 
